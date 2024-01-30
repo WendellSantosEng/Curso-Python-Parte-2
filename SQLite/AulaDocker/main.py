6a92456982ab48d0981e02b8c9a5f430e9d59679
Wendell S7 <wendellsantos7007@gmail.com>
2024-01-30 12:58:55 -0300

    mysql searchs

--- SQLite/AulaDocker/main.py
+++ SQLite/AulaDocker/main.py
@@ -4,21 +4,83 @@ import os
 
 dotenv.load_dotenv()
 
+TABLE_NAME = "customers"
+
 connection = pymysql.connect(
     host = os.environ["MYSQL_HOST"],
     user = os.environ["MYSQL_USER"],
     password = os.environ["MYSQL_PASSWORD"],
-    database = os.environ["MYSQL_DATABASE"]
+    database = os.environ["MYSQL_DATABASE"],
+    charset= "utf8mb4"
 )
 
 with connection:
     with connection.cursor() as cursor:
         cursor.execute(
-            'CREATE TABLE customers ('
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
             'id INT NOT NULL AUTO_INCREMENT, '
             'nome VARCHAR(50) NOT NULL, '
             'idade INT NOT NULL, '
             'PRIMARY KEY (id)'
             ') '
         )
+        # Isso limpa a tabela
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME} ')
         connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data = ('Leon', 18)
+        result = cursor.execute(sql, data)
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data2 = {
+            "age": 37,
+            "name": "Le",
+        }
+        result = cursor.execute(sql, data2)
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data2 = (
+            {"age": 41, "name": "Sah" },
+            {"age": 16, "name": "John" },
+            {"age": 62, "name": "Robert" },
+            )
+        result = cursor.executemany(sql, data2)
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data4 = (
+            ("Siri", 22, ),
+            ("Helena", 15, ),
+        )
+        result = cursor.executemany(sql, data4)  # type: ignore
+        print(sql)
+        print(data4)
+        print(result)
+    connection.commit()
