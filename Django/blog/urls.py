b8e36507802ef71bd8c1fd86ac755c34b9cc4da0
Wendell S7 <wendellsantos7007@gmail.com>
2024-02-24 10:24:02 -0300

    finalizando base django

--- Django/blog/urls.py
+++ Django/blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog, name="home"),
+    path('<int:post_id>/', views.post, name="post"),
     path('exemplo/', views.exemplo, name="exemplo"),
 ]
--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,4 +1,8 @@
+from typing import Any
+
+from django.http import Http404, HttpRequest
 from django.shortcuts import render
+from blog.data import posts
 
 # Create your views here.
 
@@ -6,7 +10,7 @@ def blog(request):
     print('blog')
 
     context = {
-        'text': 'Olá blog'
+        'posts': posts,
     }
 
     return render(
@@ -15,12 +19,33 @@ def blog(request):
         context
     )
 
+def post(request:HttpRequest, post_id:int):
+    found_post:dict[str,Any] | None = None
+
+    for post in posts:
+        if post["id"] == post_id:
+            found_post = post
+            break
+
+    if found_post is None:
+        raise Http404("Post não existe.")
+
+    context = {
+        'post': found_post,
+        'title': found_post['title'] + ' - '
+    }
+
+    return render(
+        request,
+        'blog/post.html',
+        context
+    )
+
 
 def exemplo(request):
     print('exemplo')
 
     context = {
-        'text': 'Olá exemplo',
         'title': 'Essa é uma página de exemplo - ',
     }
 
